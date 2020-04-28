5446b5986c996df54ebe5a0013117d4d7507e7ad
Kevin Mershon <kevin.mershon@rakuten.com>
2020-04-27 20:24:21 -0700

    Restructure project per Julie

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # Free Food Lookup Tool
 A lookup tool for Santa Clara County residents to find programs like free school meals and groceries near them
 
+## Notes from Designer
+
+[Current live site](https://gis.sanjoseca.gov/maps/covid19foodpublic/addresslookup/)
+
+[Figma mockups](https://www.figma.com/file/NrW597pIrutfoRwm3pdVLs/Food-Lookup-Tool?node-id=0%3A1)
+
+Email Julie if you have questions: julie.kim@sanjoseca.gov
+
+
+## Multi-Level Accordion Menu
+
+A simple CSS accordion menu with support for sub level items.
+
+[Article on CodyHouse](https://codyhouse.co/gem/css-multi-level-accordion-menu)
+
+[Demo](https://codyhouse.co/demo/multi-level-accordion-menu)
+
+[License](https://codyhouse.co/license)
+
+## Dependencies
+
+This experiment is built upon the [CodyHouse Framework](https://github.com/CodyHouse/codyhouse-framework).
+
+Make sure to include both the style.scss and util.js files of the framework.
+
+## Credits
+
+Icons: [Nucleo Library](https://nucleoapp.com/)
+
 ## How to Contribute
 - [Find an issue and assign yourself](https://github.com/codeforsanjose/free-food-lookup/issues)
-- Communicate with the team on Slack (channel: #silicon-valley-strong). 
+- Communicate with the team on Slack (channel: #silicon-valley-strong).
 - [If you do not already have an account on our Slack channel, join us](https://slackin-c4sj.herokuapp.com/) or [Log onto our Slack](https://codeforsanjose.slack.com/)
 - Attend a Code for San Jose civic hack night meetup: https://www.meetup.com/code-for-san-jose/
 
---
+++ assets/js/csj_addressgeocodercounty.js
@@ -0,0 +1,58 @@
+//////////////////ADDRESS SUGGESTIONS//////////////////
+function getSearchSuggestions(userTypedAddress){
+	if(userTypedAddress.length>3){
+		var request = new XMLHttpRequest()
+
+		// Open a new connection, using the GET request on the URL endpoint
+		request.open('GET', 'https://geo.sanjoseca.gov/server/rest/services/Locators/EOC_CountyLocator/GeocodeServer/suggest?text='+userTypedAddress+'&maxSuggestions=&category=&countryCode=&searchExtent=&location=&distance=&f=pjson', true)
+
+		request.onload = function() {
+			// Begin accessing JSON data here
+			var data = JSON.parse(this.response)
+			console.log(data);
+			var insertSuggestions = "";
+			for (i = 0; i < data.suggestions.length; i++) {
+				insertSuggestions += "<div class='selectSuggestion' onclick='searchForThisAddress(null, this.id)' id='" + data.suggestions[i].magicKey + "'>" + removeZipCode(data.suggestions[i].text) + "</div>";
+			}
+			insertSuggestions += "<p><table><tr><td style='vertical-align:middle;'>&nbsp;<img src='assets/img/logo_county.png' height='20px' /></td><td>&nbsp;<b>Powered By</b> Santa Clara County Address Database</td></tr></table></p>";
+			document.getElementById("addressSearchSuggest").innerHTML = insertSuggestions;
+			document.getElementById("addressSearchSuggest").style.visibility = "visible";
+		}
+
+		function removeZipCode(address){
+			address = address.substring(0, address.length-11);
+			return address;
+		}
+
+		// Send request
+		request.send()
+	}
+}
+
+//////////////////SEARCH ADDRESS//////////////////
+function searchForThisAddress(typedAddress, magicKey){
+	var request = new XMLHttpRequest()
+	
+	if(magicKey != null){ //address came from suggestion magic key
+		request.open('GET', 'https://geo.sanjoseca.gov/server/rest/services/Locators/EOC_CountyLocator/GeocodeServer/findAddressCandidates?Address=&Address2=&Address3=&Neighborhood=&City=&Subregion=&Region=&Postal=&PostalExt=&CountryCode=&SingleLine=&outFields=*&maxLocations=&matchOutOfRange=true&langCode=&locationType=&sourceCountry=&category=&location=&distance=&searchExtent=&outSR=4269&magicKey='+magicKey+'&f=pjson', true)
+	}
+	else if(typedAddress != null){ //address was typed
+		request.open('GET', 'https://geo.sanjoseca.gov/server/rest/services/Locators/EOC_CountyLocator/GeocodeServer/findAddressCandidates?Address='+typedAddress+'&Address2=&Address3=&Neighborhood=&City=&Subregion=&Region=&Postal=&PostalExt=&CountryCode=&SingleLine=&outFields=*&maxLocations=&matchOutOfRange=true&langCode=&locationType=&sourceCountry=&category=&location=&distance=&searchExtent=&outSR=4269&magicKey=&f=pjson', true)
+	}
+
+	request.onload = function() {
+		// Begin accessing JSON data here
+		var data = JSON.parse(this.response)
+		console.log(data);
+		var foundAddress = data.candidates[0].address;
+		var foundStreet = data.candidates[0].attributes.StName + "" + data.candidates[0].attributes.StType; //don't need a space, its built into TYPE field
+		var foundLat = data.candidates[0].location.y;
+		var foundLng = data.candidates[0].location.x;
+		document.getElementById("addressSearchSuggest").style.visibility = "hidden";
+		document.getElementById("addressSearch").value = foundAddress;
+		callApiAndBuildJson(foundLat,foundLng,foundAddress);
+	}
+
+	// Send request
+	request.send()
+}
